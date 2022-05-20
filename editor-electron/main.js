2f7cea8f38959988acb153f41d07bb3ce0e34a94
Diyar Ceran <diyar.ceran@devoks.com.tr>
2022-05-20 20:11:13 +0200

    last changes

--- editor-electron/main.js
+++ editor-electron/main.js
@@ -15,8 +15,8 @@ function isDev() {
 function createWindow() {    
     // Create the browser window.
     mainWindow = new BrowserWindow({
-        width: 1000,
-        height: 800,
+        width: 1200,
+        height: 900,
         webPreferences: {
             nodeIntegration: true,
             preload: path.join(__dirname, 'preload.js'),
--- editor-electron/src/App.svelte
+++ editor-electron/src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-
+import { Router, Route, Link } from "svelte-routing";
 </script>
 
 <head>
