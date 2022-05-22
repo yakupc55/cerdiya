6fa69ac9aa7fea98d3d4788e8f5aac70246bffa4
Diyar Ceran <diyar.ceran@devoks.com.tr>
2022-05-22 12:00:58 +0200

    drag and drop success test

--- editor-svelte3/src/AddFile.svelte
+++ editor-svelte3/src/AddFile.svelte
@@ -1,4 +1,26 @@
 <script>
+    document.addEventListener('drop', (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+ 
+    for (const f of event.dataTransfer.files) {
+        // Using the path attribute to get absolute file path
+        console.log('File Path of dragged files: ', f.path)
+      }
+});
+ 
+document.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  });
+ 
+document.addEventListener('dragenter', (event) => {
+    console.log('File is in the Drop Space');
+});
+ 
+document.addEventListener('dragleave', (event) => {
+    console.log('File has left the Drop Space');
+});
 </script>
 
 <h1>
