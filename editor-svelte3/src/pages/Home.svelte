47aeee18eac5297d14c70ed768de71160736450b
Diyar Ceran <diyar.ceran@devoks.com.tr>
2022-06-18 00:19:47 +0200

    last mini changes

--- editor-svelte3/src/pages/Home.svelte
+++ editor-svelte3/src/pages/Home.svelte
@@ -1,7 +1,21 @@
 <script>
 	import { createEventDispatcher } from 'svelte';
+    function startNewProject(){
+
+    }
+
+    function openProject(){
+
+    }
+
+    function saveProject(){
+
+    }
 </script>
 
+<button type="button"  class="btn btn-primary btn-block uploadButton" on:click={startNewProject}>Start New Project</button>
+<button type="button"  class="btn btn-primary btn-block uploadButton" on:click={openProject} >Open a Project</button>
+<button type="button"  class="btn btn-primary btn-block uploadButton" on:click={saveProject} >Save The Project</button>
 <h1>
     Developer: Diyar Ceran
     <br>
